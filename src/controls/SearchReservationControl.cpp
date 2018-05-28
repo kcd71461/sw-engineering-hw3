7b5b1ef8bdeb51e2458245f22efe5207c816cff1
Youngjin Kwon <youngjin@finda.co.kr>
2018-05-28 18:09:45 +0900

    예약 정보 조회 boundary / control 업데이트

---
+++ src/controls/SearchReservationControl.cpp
@@ -0,0 +1,8 @@
+//
+// Created by 권영진 on 2018. 5. 28..
+//
+#include "../boundaries/SearchReservationUI.h"
+#include "../controls/SearchReservationControl.h"
+
+GENERATE_DEFAULT_BOUNDARY_INTERFACE_IMPLEMENT(SearchReservationUI,SearchReservationControl)
+// TODO: 필요한 Boundary 함수 구현
