9b5317939fe3f4aa2f1b853456f653e0554e3a65
kim changduk <kimchangduk92@gmail.com>
2018-05-31 02:02:08 +0900

    빌드 실패 버그 수정

--- src/Host.cpp
+++ src/Host.cpp
@@ -4,12 +4,4 @@
 
 #include "Host.h"
 
-AccommodationCollection *Host::getAccommodations() {
-    AccommodationCollection *accomodations = AccommodationCollection::getInstance();
-    // AccommodationCollection *result = new AccommodationCollection();
-    for (int i = 0; i < accomodations->getSize(); i++) {
-        
-    }
-}
-
 Host::Host(const string &name, const string &securityNumber, const string &address, const string &id, const string &password) : Member(MemberTypes::HostMember, name, securityNumber, address, id, password) {}
