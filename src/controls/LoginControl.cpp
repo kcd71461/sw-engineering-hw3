09d683df72170f6915978539aa10c33d9cfc2444
kim changduk <kimchangduk92@gmail.com>
2018-05-30 22:05:01 +0900

    시퀀스그램 다이어그램 그리기 편하게 로직수정

--- src/controls/LoginControl.cpp
+++ src/controls/LoginControl.cpp
@@ -9,26 +9,26 @@ GENERATE_DEFAULT_CONTROL_INTERFACE_IMPLEMENT(LoginControl, LoginUI)
 
 void LoginControl::tryLogin(string id, string password) {
     MemberCollection *members = MemberCollection::getInstance();
-    Member *member = NULL;
+
 
     for (int i = 0, length = members->getSize(); i < length; i++) {
         if (members->get(i)->getID().compare(id) == 0) {
+            Member *member = NULL;
             member = members->get(i);
+            if (member != NULL && member->equalsPassword(password)) {
+                Session *newSession = new Session(member);
+                SessionCollection *sessions = SessionCollection::getInstance();
+                sessions->add(newSession);
+                sessions->changeCurrentSession(newSession);
+                this->getLoginUI()->printLine("> %s %s", id.c_str(), password.c_str());
+            } else {
+                if (member == NULL) {
+                    this->getLoginUI()->printLine("> 해당 ID 회원이 존재하지 않습니다.");
+                } else {
+                    this->getLoginUI()->printLine("> 패스워드가 틀립니다.");
+                }
+            }
             break;
-        }
-    }
-
-    if (member != NULL && member->equalsPassword(password)) {
-        Session *newSession = new Session(member);
-        SessionCollection *sessions = SessionCollection::getInstance();
-        sessions->add(newSession);
-        sessions->changeCurrentSession(newSession);
-        this->getLoginUI()->printLine("> %s %s", id.c_str(), password.c_str());
-    } else {
-        if (member == NULL) {
-            this->getLoginUI()->printLine("> 해당 ID 회원이 존재하지 않습니다.");
-        } else {
-            this->getLoginUI()->printLine("> 패스워드가 틀립니다.");
         }
     }
 }
